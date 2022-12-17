6909c420cb21805c0efca75b0485224dc5bdd89c
G-Alchemist <115730484+G-Alchemist@users.noreply.github.com>
2022-12-17 21:53:49 +0100

    Create the game script
    getComputerCHoice selects a random string out of an array
    capitalize changes player input cases to make the input insensitive to cases
    gameRound compares player and computer play to determine who wins the round, returning an array for score
    game creates a loop of 5, stores each round incrementation to score and determines who wins after the end of the loop

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+
+game();
+
 function getComputerChoice() {
     const computerChoice = ["Rock", "Paper", "Scissors"];
     const randomNumber = Math.floor(Math.random() * computerChoice.length);
@@ -5,4 +8,53 @@ function getComputerChoice() {
     return randomChoice;
 }
 
-console.log(getComputerChoice())
+function gameRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        console.log(`It's a tie ! You both played ${playerSelection}`);
+        return [0,0];
+    } else if ((playerSelection == "Rock" & computerSelection == "Scissors") || (playerSelection == "Paper" & computerSelection == "Rock") 
+    || (playerSelection == "Scissors" & computerSelection == "Paper")) {
+        console.log(`You won this round ! ${playerSelection} beats ${computerSelection} !`);
+        return [1,0];
+    } else if ((playerSelection == "Scissors" & computerSelection == "Rock") || (playerSelection == "Rock" & computerSelection == "Paper") 
+    || (playerSelection == "Paper" & computerSelection == "Scissors")) {
+        console.log(`You lost this round ! ${computerSelection} beats ${playerSelection} !`);
+        return [0,1];
+    } else {
+        console.log("Wrong input, you need to choose Rock, Paper or Scissors !");
+        return [0,0];
+    }
+}
+
+function capitalize(playerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    capitalizedLetter = playerSelection.charAt(0);
+    capitalizedLetter = capitalizedLetter.toUpperCase();
+    playerSelection = capitalizedLetter + playerSelection.slice(1);
+    return playerSelection;
+}
+
+function game () {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let gamecount = 0; gamecount < 5; gamecount++) {
+        let computerSelection = getComputerChoice();
+        let playerSelection = prompt("Rock, Paper or Scissors ? Make your choice !");
+        playerSelection = capitalize(playerSelection);
+        let [playerWon, computerWon] = gameRound(playerSelection, computerSelection);
+        if (playerWon == 1) {
+            playerScore++;
+        } else if (computerWon == 1) {
+            computerScore++;
+        }
+        console.log(`Your score is : ${playerScore}\nComputer score is : ${computerScore}`);
+    }
+    console.log(playerScore, computerScore);
+    if(playerScore > computerScore) {
+        console.log("Congratulations ! You won the game !");
+    } else if (playerScore < computerScore) {
+        console.log("Oh no! You lost against the computer, too bad !");
+    } else {
+        console.log("No way ! It's a tie !");
+    }
+}
